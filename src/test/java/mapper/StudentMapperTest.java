2270eccaa0b7a2f6656a7e8cb05ee22f0b850ca3
Gavin Yau <GavinYau1995@outlook.com>
2020-10-16 15:19:33 +0800

    添加mybatis3.4.4源码

--- src/test/java/mapper/StudentMapperTest.java
+++ src/test/java/mapper/StudentMapperTest.java
@@ -5,13 +5,8 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.IOException;
-
-import static org.junit.Assert.*;
 
 public class StudentMapperTest {
 
